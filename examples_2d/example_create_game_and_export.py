d7dd0d705817ca392a66feaa15dbb1b4ab0b8d7a
JessyJP <49342341+JessyJP@users.noreply.github.com>
2024-04-26 20:15:48 +0100

    Update example_game_generation.py

--- examples_2d/example_create_game_and_export.py
+++ examples_2d/example_create_game_and_export.py
@@ -1,35 +1,43 @@
+# Import necessary modules from the sudoku package
 from sudoku import *
 from sudoku.make_game import select_board_file, make_game_from_solution
 
-B = load_sudoku_board(select_board_file("../Solutions/", index=None));
+# Load a Sudoku board from a specified directory
+B = load_sudoku_board(select_board_file("../Solutions/", index=None))
+print_board_state(B, substituteZero=".", border=True, clear=True)
 
-
-print_board_state(B, ".", border=True, clear=True)
+# Initialize variables to keep track of the board with the least number of clues
 B_least = B.copy()
-for t in range(300):# try 10 times
-    try:
-        B_current = make_game_from_solution(B.copy())
-    except:
-        continue
-    currentSum = sum(sum(B_current>0))
-    newSum = sum( sum(B_least>0))
+
+# Attempt to generate a game with fewer clues multiple times to find the best one
+for t in range(300):  # Try 300 times
+
+    # Choose between the 2 options.
+    B_current = make_game_from_solution(B.copy())
+    # B_current = make_game_from_solution_sequentially(B.copy())
+
+    currentSum = count_non_zero_elements(B_current)
+    newSum = count_non_zero_elements(B_least)
     if currentSum < newSum:
         print(f"TEST {t}: Current number count {currentSum} < Smallest number count {newSum}")
         B_least = B_current
-B = B_least
 
-print_board_state(B, ".", border=True, clear=False)
-# save_sudoku_board(B,"Games")
+# Display the board with the least clues found
+print_board_state(B_least, substituteZero=".", border=True, clear=False)
 
-directory = r"r:/"
-filename = r"sudoku"
-fileExt = r".tex"
-export_board_to_TEX(directory+filename+fileExt,B)
+# Save the board to different formats
+directory = "r:/"
+filename = "sudoku"
+fileExt = ".tex"
 
-compilePDF(directory,filename+fileExt)
+# Export and compile the board to a LaTeX document
+export_board_to_TEX(directory + filename + fileExt, B_least)
+compilePDF(directory, filename + fileExt)
 
-html_file = directory+filename+".html"
-export_board_to_html(B, html_file)
-
+# Export the board to an HTML file and open it in a web browser
+html_file = directory + filename + ".html"
+export_board_to_html(B_least, html_file)
 open_html(html_file)
-open_pdf(directory+filename+r".pdf")
+
+# Open the generated PDF document
+open_pdf(directory + filename + ".pdf")
