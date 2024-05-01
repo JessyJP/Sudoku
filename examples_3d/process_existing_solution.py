6f30e86c0a6a2730b3a29012f93d1eddfdd470e4
JessyJP <49342341+JessyJP@users.noreply.github.com>
2024-05-01 21:51:55 +0100

    Update process_existing_solution.py

--- examples_3d/process_existing_solution.py
+++ examples_3d/process_existing_solution.py
@@ -1,4 +1,4 @@
-from sudoku import compilePDF, open_html, open_pdf
+from sudoku import compilePDF, open_html, open_pdf, NumberVoxel
 from sudoku.visualize import update_voxel_colors
 from sudoku3d.export_3d import load_sudoku_board_3d, export_board_to_TEX_3d, export_3d_board_to_TEX, \
     export_board_to_html_3d
@@ -34,6 +34,24 @@ compilePDF(directory, filename + fileExt)
 html_file = directory + filename + ".html"
 export_board_to_html_3d(B, html_file)
 # open_html(html_file)
+
+
+# TODO: move this method somewhere appropriate
+def save_figure_as_png(filename="output.png"):
+    """
+    Saves the current 3D plot to a PNG file.
+
+    Parameters:
+    - filename (str): The name of the file where the plot will be saved.
+    """
+    NumberVoxel.fig.savefig(filename, dpi=300)  # Save the figure as a PNG file with 300 dpi
+    print(f"Figure saved as {filename}")
+
+# Export PNG
+ax = NumberVoxel.ax
+ax.set_axis_off()
+save_figure_as_png(directory+filename+".png")
+
 
 # Open the generated PDF document
 open_pdf(directory + filename + ".pdf")
